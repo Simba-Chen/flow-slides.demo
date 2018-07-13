9c85b586e0897f5bc078c5d7987c31f28f6b6c8a
chenxinda <chenxinda520@qq.com>
2018-07-13 17:45:32 +0800

    无缝轮播3.0

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-$('.images>img:nth-child(1)').addClass('current')
-$('.images>img:nth-child(2)').addClass('right')
-$('.images>img:nth-child(3)').addClass('right')
-$('.images>img:nth-child(4)').addClass('right')
-$('.images>img:nth-child(5)').addClass('right')
-$('.images>img:nth-child(6)').addClass('right')
-let n = 1
 
+let n = 1
+init()
+ 
+
+setInterval(function (){
+    $(`.images>img:nth-child(${num(n)})`).addClass('left').removeClass('current').removeClass('right')
+    .one('transitionend',function (message){
+        $(message.currentTarget).removeClass('left').addClass('right').removeClass('current')
+    })
+    $(`.images>img:nth-child(${num(n+1)})`).addClass('current').removeClass('right').removeClass('left')
+    n++
+    console.log('n的值'+ num(n))
+    console.log('n+1的值'+ num(n+1))
+},3000)
+function init(){
+    $(`.images>img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
 function num(n){
     if(n>6){
         n = n%6
@@ -15,13 +26,4 @@ function num(n){
     }//n = 1,2,3,4,5,6
     return n 
 }
-setInterval(function (){
-    $(`.images>img:nth-child(${num(n)})`).addClass('left').removeClass('current')
-    .one('transitionend',function (message){
-        $(message.currentTarget).removeClass('left').addClass('right')
-    })
-    $(`.images>img:nth-child(${num(n+1)})`).addClass('current').removeClass('right')
-    n++
-    console.log(num(n))
-},1000)
 
