3f6107e02d08dac986f63b5aafafd2dafd9d09ce
chenxinda <chenxinda520@qq.com>
2018-07-13 16:38:09 +0800

    无缝轮播2.0

--- main.js
+++ main.js
@@ -4,23 +4,24 @@ $('.images>img:nth-child(3)').addClass('right')
 $('.images>img:nth-child(4)').addClass('right')
 $('.images>img:nth-child(5)').addClass('right')
 $('.images>img:nth-child(6)').addClass('right')
-setTimeout(function (){
-    $('.images>img:nth-child(1)').addClass('left').removeClass('current')
-    $('.images img:nth-child(2)').addClass('current').removeClass('right')
+let n = 1
+
+function num(n){
+    if(n>6){
+        n = n%6
+        if(n === 0){
+            n = 6
+        }
+    }//n = 1,2,3,4,5,6
+    return n 
+}
+setInterval(function (){
+    $(`.images>img:nth-child(${num(n)})`).addClass('left').removeClass('current')
+    .one('transitionend',function (message){
+        $(message.currentTarget).removeClass('left').addClass('right')
+    })
+    $(`.images>img:nth-child(${num(n+1)})`).addClass('current').removeClass('right')
+    n++
+    console.log(num(n))
 },1000)
-setTimeout(function (){
-    $('.images>img:nth-child(2)').addClass('left').removeClass('current')
-    $('.images img:nth-child(3)').addClass('current').removeClass('right')
-},2000)
-setTimeout(function (){
-    $('.images>img:nth-child(3)').addClass('left').removeClass('current')
-    $('.images img:nth-child(4)').addClass('current').removeClass('right')
-},3000)
-setTimeout(function (){
-    $('.images>img:nth-child(4)').addClass('left').removeClass('current')
-    $('.images img:nth-child(5)').addClass('current').removeClass('right')
-},4000)
-setTimeout(function (){
-    $('.images>img:nth-child(5)').addClass('left').removeClass('current')
-    $('.images img:nth-child(6)').addClass('current').removeClass('right')
-},5000)
+
